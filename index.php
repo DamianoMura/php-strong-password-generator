6a1b0eb25eb2d2c9dd7b06d224b0c7467b003f80
DamianoMura <mura.damiano14@gmail.com>
2025-10-30 15:23:25 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,22 @@
+<?php
+session_start();
+$_SESSION['username'] = 'xtreme_user';
+var_dump($_SESSION);
+?>
+
+
+
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>XTREME PASSWORD GENERATOR</title>
+</head>
+
+<body>
+
+</body>
+
+</html>
